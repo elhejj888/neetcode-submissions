231a6ea70ee8446d90a02a07e401881d53eb2e69
Youssef el hejjioui <117082947+elhejj888@users.noreply.github.com>
2026-07-31 16:44:34 +0100

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.go
@@ -0,0 +1,25 @@
+import("slices")
+func insert(intervals [][]int, newInterval []int) [][]int {
+
+	res := [][]int{}
+
+	
+	
+	for i:=0 ; i < len(intervals); i++ {
+		if newInterval[1] < intervals[i][0] {
+			res = append(res, newInterval)
+			return slices.Concat(res,intervals[i:])
+		} else if newInterval[0] > intervals[i][1]{
+			res = append(res, intervals[i])
+		} else {
+			newInterval = [] int {min(newInterval[0], intervals[i][0]), max(newInterval[1],intervals[i][1])}
+		}
+		
+	}
+
+	res = append(res, newInterval)
+	
+	
+	return res
+
+}
