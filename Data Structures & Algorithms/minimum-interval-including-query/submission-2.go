e5e80325f326079036f6418097d696d6260564b0
Youssef el hejjioui <117082947+elhejj888@users.noreply.github.com>
2026-08-03 12:56:09 +0100

    Add: minimum-interval-including-query - submission-2

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-2.go
@@ -0,0 +1,32 @@
+func minInterval(intervals [][]int, queries []int) []int {
+    
+	sort.Slice(intervals, func(i,j int)bool {
+		return intervals[i][0] <= intervals[j][0]
+	})
+
+	minInterval := -1
+	
+	res := make([]int,len(queries))
+	for i:=0; i<len(queries); i++{
+
+		for j:= 0; j<len(intervals); j++{
+			if queries[i] <= intervals[j][1] && queries[i] >= intervals[j][0]{
+				interval := intervals[j][1] - intervals[j][0] + 1
+				if minInterval != -1 {
+					if interval < minInterval {
+						minInterval = interval
+					}
+				} else {
+					minInterval = interval
+				}
+			} else if queries[i] < intervals[j][0]{
+				break
+			}
+		}
+
+		res[i] = minInterval
+		minInterval = -1
+
+	}
+	return res
+}
