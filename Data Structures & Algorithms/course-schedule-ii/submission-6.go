7f2081ab89a0aa4ba714cf8488caa2a70b0ceeca
Youssef el hejjioui <117082947+elhejj888@users.noreply.github.com>
2026-09-01 22:39:57 +0100

    Add: course-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-6.go
@@ -0,0 +1,42 @@
+func findOrder(numCourses int, grid [][]int) []int {
+	res := []int{}
+    rows := len(grid)
+    seen := make(map[int]int)
+    adj := make(map[int][]int)
+    for i:=0; i<rows; i++ {
+
+        adj[grid[i][0]] = append(adj[grid[i][0]], grid[i][1])
+    }
+	
+    var dfs func(course int) bool
+
+    dfs = func(course int) bool {
+        if seen[course] == 1 {
+            return false 
+        }
+        if seen[course] == 2 {
+            return true
+        } 
+
+        
+        seen[course] = 1
+        for _, v := range adj[course]{
+
+            if !dfs(v) {
+                return false
+            }
+        }
+        seen[course] = 2
+		res = append(res,course)
+        return true
+    }
+    for i:=0; i<numCourses; i++ {
+        if !dfs(i) {
+            return []int{}
+        }
+    }
+
+	return res
+
+}
+
