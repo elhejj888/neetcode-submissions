2821bbaa27168599e36e067cecd7f2697535e685
Youssef el hejjioui <117082947+elhejj888@users.noreply.github.com>
2026-09-02 19:07:22 +0100

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.go
@@ -0,0 +1,25 @@
+func validTree(n int, edges [][]int) bool {
+    //rows := len(edges)
+	if len(edges) != n - 1 {
+		return false
+	}
+	seen := make(map[int]bool)
+	adj := make(map[int][]int)
+	for _,v := range edges {
+		adj[v[0]] = append(adj[v[0]],v[1])
+		adj[v[1]] = append(adj[v[1]],v[0])
+	}
+	var dfs func(int)
+	dfs = func(node int) {
+		if seen[node]{
+			return
+		}
+		seen[node] = true
+		for _,v := range adj[node]{
+
+			dfs(v)
+		}
+	}
+	dfs(0)
+	return len(seen) == n
+}
