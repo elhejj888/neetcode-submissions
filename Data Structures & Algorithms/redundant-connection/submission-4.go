a62a083e5da56483fd8cbbe4d7c9a81250d994a2
Youssef el hejjioui <117082947+elhejj888@users.noreply.github.com>
2026-09-03 23:36:21 +0100

    Add: redundant-connection - submission-4

---
+++ Data Structures & Algorithms/redundant-connection/submission-4.go
@@ -0,0 +1,23 @@
+func findRedundantConnection(edges [][]int) []int {
+	parent := make(map[int]int)
+	for i := 1; i<=len(edges); i++ {
+		parent[i] = i
+	}
+	var find func(int)int
+	find = func(x int)int{
+
+		if x != parent[x]{
+			parent[x] = find(parent[x])  
+    	}
+    return parent[x]
+	}
+	res := []int{}
+	for _,v := range edges {
+		if find(v[0]) == find(v[1]) {
+			res = v
+			break
+		}
+		parent[find(v[0])] = find(v[1])
+	}
+	return res
+}
