a3b6a184c79dde46d2f6867af38fce77dc8dddb1
Youssef el hejjioui <117082947+elhejj888@users.noreply.github.com>
2026-09-25 23:50:37 +0100

    Add: longest-palindromic-substring - submission-9

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-9.go
@@ -0,0 +1,45 @@
+func longestPalindrome(s string) string {
+
+	if len(s) < 2 || ( len(s) == 2 && s[0] != s[1] ){
+		return string(s[0])
+	}
+
+	var res string
+
+	for i:=0; i<len(s); i++{
+		k, j, d := i, len(s)-1,0
+		start,end := "",""
+		for k<j {
+			if s[i] == s[j] && start != ""{
+				d = j
+			}
+			if s[k] == s[j]{
+				start += string(s[k])
+				end = string(s[j]) + end
+				k++
+				j--
+			}else {
+				j--
+			}
+			if s[k] != s[j] && start != ""{
+				start = ""
+				end =""
+				k=i
+				if d != 0 {
+					j = d
+					d = 0
+				}
+			}
+			if k == j && start != "" {
+				start += string(s[k])
+			}
+			
+		}
+
+		if len(start+end) > len(res) {
+			res = start+end
+		}
+	}
+
+	return res
+}
